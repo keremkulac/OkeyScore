372cff6fdee39b43cc344479cac0934c329097fb
Kerem Kulaç <keremkulac14@gmail.com>
2025-05-30 22:10:33 +0300

    finishedPartnerGame için yeni tasarım ve kod düzenlemeleri yapıldı

---
+++ app/src/main/java/com/keremkulac/okeyscore/presentation/ui/Deneme2ViewModel.kt
@@ -0,0 +1,77 @@
+package com.keremkulac.okeyscore.presentation.ui
+
+
+import android.content.Context
+import android.widget.EditText
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.keremkulac.okeyscore.R
+import com.keremkulac.okeyscore.data.repository.OkeyScoreRepositoryImp
+import com.keremkulac.okeyscore.model.FinishedPartnerGame
+import com.keremkulac.okeyscore.model.Info
+import com.keremkulac.okeyscore.model.Player
+import com.keremkulac.okeyscore.util.InputValidation
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import javax.inject.Inject
+
+@HiltViewModel
+class Deneme2ViewModel @Inject constructor(
+    private val okeyScoreRepositoryImp: OkeyScoreRepositoryImp,
+    private val inputValidation: InputValidation
+) :
+    ViewModel() {
+
+    private val _validationMessage = MutableLiveData<String>()
+    val validationMessage: LiveData<String> get() = _validationMessage
+
+    fun savePartnerGame(finishedPartnerGame: FinishedPartnerGame) {
+        viewModelScope.launch {
+            okeyScoreRepositoryImp.insertFinishedPartnerGame(finishedPartnerGame)
+        }
+    }
+
+    fun checkPlayerNames(playerNames: List<String>): Boolean {
+        return inputValidation.isAllUsernamesFilled(playerNames) { message ->
+            _validationMessage.value = message
+        }
+    }
+
+    fun checkTeamNames(teamNames: List<String>): Boolean {
+        return inputValidation.isAllUsernamesFilled(teamNames) { message ->
+            _validationMessage.value = message
+        }
+    }
+
+    fun sameNamesCheck(playerNames: List<String>): Boolean {
+        return inputValidation.checkSamePlayerNames(playerNames) { message ->
+            _validationMessage.value = message
+        }
+    }
+
+    fun checkAllRoundScoreFilled(roundScores: List<EditText>,lineCount : Int): Boolean {
+        return inputValidation.isAllRoundFieldsFilled(roundScores,lineCount) { message ->
+            _validationMessage.value = message
+        }
+    }
+
+    fun createInfo(player: List<Player>, context: Context): Info {
+
+        val minScorePlayer = player.minBy { it.totalScore.toInt() }
+        return Info(
+            context.getString(R.string.winning_player_info)
+                .format(minScorePlayer.name, minScorePlayer.totalScore), getCurrentDate()
+        )
+    }
+
+    private fun getCurrentDate(): String {
+        val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")
+        return ZonedDateTime.now(ZoneId.of("Asia/Istanbul")).toLocalDateTime().format(formatter)
+    }
+
+}
