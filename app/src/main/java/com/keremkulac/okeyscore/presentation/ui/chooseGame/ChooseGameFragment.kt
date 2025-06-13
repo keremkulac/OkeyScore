204965955edf0f0358ec9a69217235f6e86c6a03
Kerem Kulaç <keremkulac14@gmail.com>
2025-06-13 14:09:30 +0300

    Seçim ekranı tasarımında değişilik yapıldı

--- app/src/main/java/com/keremkulac/okeyscore/presentation/ui/chooseGame/ChooseGameFragment.kt
+++ app/src/main/java/com/keremkulac/okeyscore/presentation/ui/chooseGame/ChooseGameFragment.kt
@@ -3,6 +3,8 @@ package com.keremkulac.okeyscore.presentation.ui.chooseGame
 import android.os.Bundle
 import android.view.View
 import androidx.activity.addCallback
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.keremkulac.okeyscore.R
 import com.keremkulac.okeyscore.databinding.FragmentChooseGameBinding
@@ -12,25 +14,78 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class ChooseGameFragment :
     BaseFragment<FragmentChooseGameBinding>(FragmentChooseGameBinding::inflate) {
+    private var selectedGame: String = ""
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        goToSaveSingleGameFragment()
-        goToSavePartnerGameFragment()
         setBottomNavigationVisible()
-        goToSettingsFragment()
+        setupClickListeners()
         onBackPressCancel()
     }
 
-    private fun goToSaveSingleGameFragment() {
-        binding.goToSaveSingleGameFragment.setOnClickListener {
-            findNavController().navigate(ChooseGameFragmentDirections.actionChooseGameFragmentToSaveSingleGameFragment())
+
+    private fun setupClickListeners() {
+        binding.cardViewSingleGame.setOnClickListener {
+            selectGame(getString(R.string.single_game))
+        }
+
+        binding.cardViewPartnerGame.setOnClickListener {
+            selectGame(getString(R.string.partner_game))
+        }
+
+        binding.btnStart.setOnClickListener {
+            startSelectedFragment()
         }
     }
 
-    private fun goToSavePartnerGameFragment() {
-        binding.goToSavePartnerGameFragment.setOnClickListener {
-            findNavController().navigate(ChooseGameFragmentDirections.actionChooseGameFragmentToSavePartnerGameFragment())
+    private fun selectGame(selectedGame: String) {
+        this.selectedGame = selectedGame
+        updateSelection(selectedGame)
+        when (selectedGame) {
+            getString(R.string.single_game) -> animateCardSelection(binding.cardViewSingleGame)
+            getString(R.string.partner_game) -> animateCardSelection(binding.cardViewPartnerGame)
+        }
+    }
+
+    private fun updateSelection(selectedInstrument: String) {
+        binding.singleGameIndicator.background =
+            ContextCompat.getDrawable(requireContext(), R.drawable.selection_indicator_inactive)
+        binding.partnerGameIndicator.background =
+            ContextCompat.getDrawable(requireContext(), R.drawable.selection_indicator_inactive)
+
+        when (selectedInstrument) {
+            getString(R.string.single_game) -> binding.singleGameIndicator.background =
+                ContextCompat.getDrawable(requireContext(), R.drawable.selection_indicator)
+
+            getString(R.string.partner_game) -> binding.partnerGameIndicator.background =
+                ContextCompat.getDrawable(requireContext(), R.drawable.selection_indicator)
+        }
+    }
+
+    private fun animateCardSelection(cardView: CardView) {
+        cardView.animate()
+            .scaleX(0.95f)
+            .scaleY(0.95f)
+            .setDuration(100)
+            .withEndAction {
+                cardView.animate()
+                    .scaleX(1.0f)
+                    .scaleY(1.0f)
+                    .setDuration(100)
+                    .start()
+            }
+            .start()
+    }
+
+    private fun startSelectedFragment() {
+        when (selectedGame) {
+            getString(R.string.single_game) -> findNavController().navigate(
+                ChooseGameFragmentDirections.actionChooseGameFragmentToSaveSingleGameFragment()
+            )
+
+            getString(R.string.partner_game) -> findNavController().navigate(
+                ChooseGameFragmentDirections.actionChooseGameFragmentToSavePartnerGameFragment()
+            )
         }
     }
 
@@ -45,9 +100,4 @@ class ChooseGameFragment :
         bottomNavigation.visibility = View.VISIBLE
     }
 
-    private fun goToSettingsFragment() {
-        binding.settings.setOnClickListener {
-            findNavController().navigate(ChooseGameFragmentDirections.actionChooseGameFragmentToSettingsFragment())
-        }
-    }
 }
